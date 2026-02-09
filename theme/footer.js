95f75d090882c9ff9ec8a4cf4fd7214b61217b3b
Alexey Kopytko <alexey@kopytko.com>
2026-02-09 19:44:19 +0900

    Tweaked footer

--- theme/footer.js
+++
@@ -1,6 +0,0 @@
-(function() {
-    var footer = document.createElement('footer');
-    footer.className = 'site-footer';
-    footer.innerHTML = '<p>Content licensed under <a href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a></p>';
-    document.querySelector('.content').appendChild(footer);
-})();
